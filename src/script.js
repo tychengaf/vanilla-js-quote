f929d7eccabb68ff082dd43d4e56e4320de671ae
Tony Cheng <tychengaf@gmail.com>
2023-01-01 17:00:34 +0800

    replace img src

--- src/script.js
+++ src/script.js
@@ -3,25 +3,25 @@ const data = [
     author: 'Susan Smith',
     jobTitle: 'Web Developer',
     description: "I'm baby meggings twee heigh goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoddie, cold-pressed four dollar toast everyday carry",
-    avatar: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    avatar: '../images/person-1.jpeg',
   },
   {
     author: 'Anna Johnson',
     jobTitle: 'Web Designer',
     description: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    avatar: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    avatar: '../images/person-2.jpeg',
   },
   {
     author: 'Peter Jones',
     jobTitle: 'Intern',
     description: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    avatar: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    avatar: '../images/person-3.jpeg',
   },
   {
     author: 'Bill Anderson',
     jobTitle: 'The Boss',
     description: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
-    avatar: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    avatar: '../images/person-4.jpeg',
   },
 ]
 
